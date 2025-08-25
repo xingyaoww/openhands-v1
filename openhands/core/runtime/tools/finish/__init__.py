7286d2f1340bc863c85cdd7da0141b0c7342aff9
Xingyao Wang <xingyao@all-hands.dev>
2025-08-24 22:54:38 -0400

    add finish tool

---
+++ openhands/core/runtime/tools/finish/__init__.py
@@ -0,0 +1,3 @@
+from .definition import finish_tool, FinishAction
+
+__all__ = ["finish_tool", "FinishAction"]
--- openhands/core/runtime/tools/str_replace_editor/__init__.py
+++ openhands/core/runtime/tools/str_replace_editor/__init__.py
@@ -1,10 +1,15 @@
-from .definition import str_replace_editor_tool, StrReplaceEditorAction
+from .definition import (
+    str_replace_editor_tool,
+    StrReplaceEditorAction,
+    StrReplaceEditorObservation,
+)
 from .impl import file_editor, FileEditorExecutor
 
 
 __all__ = [
     "str_replace_editor_tool",
     "StrReplaceEditorAction",
+    "StrReplaceEditorObservation",
     "file_editor",
     "FileEditorExecutor",
 ]
